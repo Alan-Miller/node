2d8cb0c0022d4911d1eaf684a3cd24269aa961ef
Alan-Miller <alan.ashman@gmail.com>
2017-11-13 20:13:51 -0700

    readme edits

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+var lodash = require('lodash');
+
+var forestAnimals = ['bear', 'squirrel', 'wolf', 'owl', 'fox', 'deer', 'wolf'];
+
+console.log(lodash.sample(forestAnimals));
+console.log(lodash.sample(forestAnimals));
+console.log(lodash.sample(forestAnimals));
+console.log(lodash.sample(forestAnimals));
+console.log(lodash.sample(forestAnimals));
